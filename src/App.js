376ad7090f17bc5be90164d82a4010d82fbaa07a
Aishwarya <aishsahoo5@gmail.com>
2019-12-03 19:34:22 +0530

    firebase authentication completed

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Header from './components/header/header.component';
 
 import { Route } from 'react-router-dom';
 import { Switch } from 'react-router-dom';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 
 
@@ -26,9 +26,32 @@ class App extends React.Component {
 
     componentDidMount() {
         this.unsubscribeFromAuth=auth.onAuthStateChanged(
-            user => {
-                this.setState({ currentUser: user });
-                console.log(user);
+            async userAuth => { 
+
+                if(userAuth) {
+                    const userRef = await createUserProfileDocument(userAuth);
+
+                    userRef.onSnapshot(snapShot=>{
+                        this.setState(
+                            {
+                                currentUser:{
+                                    id:snapShot.id,
+                                    ...snapShot.data()
+                                }
+                            }
+                        );
+                    
+                console.log(this.state);
+                    });
+                    
+                }
+
+                else {
+
+                    this.setState({currentUser: userAuth});
+
+                }         
+                
 
             });
 
